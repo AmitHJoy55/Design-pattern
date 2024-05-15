c51d22b4c3e5f5e4da75a79df6dfb659ef4406f8
Amit Hassan Joy <108347615+AmitHJoy55@users.noreply.github.com>
2024-05-15 22:13:38 +0600

    Update VegetarianPizza.java

--- Pizza_Toppings_with_abstract_method/VegetarianPizza.java
+++ Pizza_Toppings_with_abstract_method/VegetarianPizza.java
@@ -5,7 +5,6 @@ public class VegetarianPizza extends Pizza {
     @Override
     public void addTopping(String topping) {
 
-        // Pepperoni pizza has pepperoni, cheese, and tomato as toppings
         System.out.println("Adding " + topping + " to Vegetarian pizza");
     }
 }
