45289ef07be9b49fca4f38df4015063dd18e2956
Amit Hassan Joy <108347615+AmitHJoy55@users.noreply.github.com>
2024-05-15 22:13:28 +0600

    Update PepperoniPizza.java

--- Pizza_Toppings_with_abstract_method/PepperoniPizza.java
+++ Pizza_Toppings_with_abstract_method/PepperoniPizza.java
@@ -3,8 +3,6 @@ package Pizza_Toppings_with_abstract_method;
 public class PepperoniPizza extends Pizza {
     @Override
     public void addTopping(String topping) {
-
-        // Pepperoni pizza has pepperoni, cheese, and tomato as toppings
         System.out.println("Adding " + topping + " to Base pizza");
     }
 }
